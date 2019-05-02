fd1cec66f183ecbd9ecf0e32606deadbde6590d5
WangTingZheng <2251985371@qq.com>
2019-05-02 21:45:34 +0800

    v1.1
    改动项目后工程文件发生了改动

--- 房间1，没完成/wifi-gramamer.c
+++ 房间1，没完成/wifi-gramamer.c
@@ -19,26 +19,22 @@ void Delay2(unsigned long cnt)
 }
 void Tranfer(uchar *s)
 {
- 
    while(*s!='\0')
    {
      SBUF= toascii(*s);
-	 while(!TI);
-	 TI=0;
+		 while(!TI);
+		 TI=0;
      s++;
-	 Delay2(1);
+		 Delay2(1);
    }
-
 }						 
-
-
 /* *******************配置wifi******************/
 void set_wifi(void)
 {
-    write(0,0x80);
+  write(0,0x80);
 	print("系统初始化...");
-    Delay2(500);
-    Tranfer("AT+RST\r\n");
+  Delay2(500);
+  Tranfer("AT+RST\r\n");
 	write(0,0x80);
 	print("系统初始化....");	
 	Delay2(1000);
@@ -46,23 +42,22 @@ void set_wifi(void)
 	write(0,0x80);
 	print("系统初始化失败");
 	Delay2(500);
-    Tranfer("AT+CIPSTART=\"TCP\",\"192.168.4.1\",5000\r\n");
+  Tranfer("AT+CIPSTART=\"TCP\",\"192.168.4.1\",5000\r\n");
 	write(0,0x80);
 	print("应该成功了~~");
 	Delay2(1000);
-    Tranfer("AT+CIPMODE=1\r\n");	 //透传模式
+  Tranfer("AT+CIPMODE=1\r\n");	 //透传模式
 	Delay2(500);
 	Tranfer("AT+CIPSEND=10\r\n");
 	Delay2(500);
 	write(0,0x80);
-	print("接收数据中....");
-    
+	print("接收数据中....");  
 }
 /********************系统初始化**************************/
 void SysInit()
 {
-   lcdinit();
-    SCON=0X50;			//设置为工作方式1
+  lcdinit();
+  SCON=0X50;			//设置为工作方式1
 	TMOD=0X21;			//设置计数器工作方式1
 	TH1=0XFD;		    //计数器初始值设置，注意波特率是9600
 	TL1=0XFD;
@@ -72,9 +67,8 @@ void SysInit()
 	ET0=1;						    //打开计数器
 	set_wifi();						//打开接收中断
 	EA=1;					//打开总中断
-	ES=1;
-	 
-U8checkdata=0;	
+	ES=1; 
+	U8checkdata=0;	
 }
 
 /******************************显示函数接收到数据才显示操场并打开定时器**************************************************/
@@ -126,7 +120,7 @@ void Dealdata()
     uchar a,b,c,d,e,f;
 	if(U8checkdata)
 	{
-       U8T_data_H%=100;
+     U8T_data_H%=100;
 	   a=U8T_data_H/10;
 	   b=U8T_data_H%10;
 	   c=U8T_data_L%10;
@@ -135,13 +129,13 @@ void Dealdata()
 	   e=U8RH_data_H%10;
 	   f=U8RH_data_L%10;
      Display(a,b,c,d,e,f);
-	 SendData(a,b,c,d,e,f);
+	   SendData(a,b,c,d,e,f);
    }
 }
 void SendData(uchar a,uchar b,uchar c,uchar d,uchar e,uchar f)
 {
-       ES=0;
-       Tranfer("R1");
+     ES=0;
+     Tranfer("R1");
 	   SBUF=a;   Delay2(1);
 	   SBUF=b;	 Delay2(1);
 	   SBUF='.'; Delay2(1);
@@ -155,7 +149,7 @@ void SendData(uchar a,uchar b,uchar c,uchar d,uchar e,uchar f)
 /************************主函数****************************/
 void main()
 {
-   SysInit(); 
+  SysInit(); 
 	while(1)
 	{
 	  /*一直在采集信息，采集到信息 发送数据*/
@@ -167,9 +161,9 @@ void main()
 /*********************定时器函数****************************/
 void time0() interrupt 1
 {
-    TH0=0X3C;
+  TH0=0X3C;
 	TL0=0XB0;
-    time++;
+  time++;
 	if(time==40)
 	{
 	  flag=~flag;
@@ -179,29 +173,27 @@ void time0() interrupt 1
 /*********************中断函数****************************/
 void Usart() interrupt 4
 {
-   TR0=0; 
+  TR0=0; 
 	if(RI)
 	{
-	   RI=0;
-	   RX1_Buffer[RX1_Cnt]=SBUF;	//接收缓冲   接收完数据显示
-	   if(RX1_Buffer[0]==0X52)	// R
-	   {
+		RI=0;
+	  RX1_Buffer[RX1_Cnt]=SBUF;	//接收缓冲   接收完数据显示
+	  if(RX1_Buffer[0]==0X52)	// R
+	  {
 			RX1_Cnt++;
-	   }
+	  }
 		else
-		{
-		    
+		{    
 			RX1_Cnt=0;
 		}
 		if(RX1_Cnt>=12)
 		{
 		  if(RX1_Buffer[1]==0x31)
 		  {
-		     RX1_Cnt=0;
-			 flag=1;
+		    RX1_Cnt=0;
+				flag=1;
 		  }
-		  else
-		   RX1_Cnt=0; 	 	   
+		  else RX1_Cnt=0; 	 	   
 		}   
 	}
 	TR0=1;
